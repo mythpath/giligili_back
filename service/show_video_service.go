a114bee6c2ad3dca9d1eda5aa2c9b890f34e30ed
liyx <liyx@yidingyun.com>
2019-08-11 21:22:50 +0800

    init giligili_back

---
+++ service/show_video_service.go
@@ -0,0 +1,27 @@
+package service
+
+import (
+	"selfText/giligili_back/model"
+	"selfText/giligili_back/serializer"
+)
+
+// ShowVideoService 投稿详情的服务
+type ShowVideoService struct {
+}
+
+// Show 视频
+func (service *ShowVideoService) Show(id string) serializer.Response {
+	var video model.Video
+	err := model.DB.First(&video, id).Error
+	if err != nil {
+		return serializer.Response{
+			Status: 404,
+			Msg:    "视频不存在",
+			Error:  err.Error(),
+		}
+	}
+
+	return serializer.Response{
+		Data: serializer.BuildVideo(video),
+	}
+}
