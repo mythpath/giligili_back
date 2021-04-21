67607ef0cc2473e749df77f2218afe9010cfe95a
liyx <liyuxin3989@sina.com>
2021-04-21 19:25:22 +0800

    update fileServer function

--- service/file/fileServer.go
+++ service/file/fileServer.go
@@ -17,6 +17,9 @@ type FileServer struct {
 
 // GetFilesAndDirs 依据指定路径遍历文件
 func (f *FileServer) GetFilesAndDirs(dirPath, filePattern string, depth int) (files, dirs []string, err error) {
+	if depth == 0 {
+		dirs = []string{dirPath}
+	}
 	dir, err := ioutil.ReadDir(dirPath)
 	if err != nil {
 		return nil, nil, err
@@ -25,8 +28,11 @@ func (f *FileServer) GetFilesAndDirs(dirPath, filePattern string, depth int) (fi
 
 	for _, file := range dir {
 		if file.IsDir() {
+			if depth == 0 {
+				continue
+			}
 			dirs = append(dirs, dirPath+pathSep+file.Name())
-			if depth > 0 {
+			if depth > 1 {
 				nextFiles, nextDirs, nextErr := f.GetFilesAndDirs(dirPath+pathSep+file.Name(), filePattern, depth-1)
 				if nextErr != nil {
 					return files, dirs, nextErr
@@ -39,6 +45,61 @@ func (f *FileServer) GetFilesAndDirs(dirPath, filePattern string, depth int) (fi
 				continue
 			}
 			files = append(files, dirPath+pathSep+file.Name())
+		}
+	}
+
+	return
+}
+
+// GetLimitFilesAndDirs 获取指定文件夹下所有文件和文件夹路径
+func (f *FileServer) GetLimitFilesAndDirs(dirPath, filePattern string) (files, dirs []string, err error) {
+	return f.GetFilesAndDirs(dirPath,filePattern,1)
+}
+
+// GetAllFiles 层级遍历指定文件夹下所有文件，获取其中所有文件路径
+func (f *FileServer) GetAllFiles(dirPath, filePattern string) (files []string, err error) {
+	dir, err := ioutil.ReadDir(dirPath)
+	if err != nil {
+		return nil, err
+	}
+	pathSep := string(os.PathSeparator)
+
+	for _, file := range dir {
+		if file.IsDir() {
+			nextFiles, nextErr := f.GetAllFiles(dirPath+pathSep+file.Name(), filePattern)
+			if nextErr != nil {
+				return files, nextErr
+			}
+			files = append(files, nextFiles...)
+		} else {
+			files = append(files, dirPath+pathSep+file.Name())
+		}
+	}
+
+	return
+}
+
+// GetAllDirs 层级遍历指定文件夹下所有文件夹路径
+func (f *FileServer) GetAllDirs(dirPath string, depth int) (dirs []string, err error) {
+	if depth == 0 {
+		return []string{dirPath}, nil
+	}
+	dir, err := ioutil.ReadDir(dirPath)
+	if err != nil {
+		return nil, err
+	}
+	pathSep := string(os.PathSeparator)
+
+	for _, file := range dir {
+		if file.IsDir() {
+			dirs = append(dirs, dirPath+pathSep+file.Name())
+			if depth > 1 {
+				nextDirs, nextErr := f.GetAllDirs(dirPath+pathSep+file.Name(), depth-1)
+				if nextErr != nil {
+					return dirs, nextErr
+				}
+				dirs = append(dirs, nextDirs...)
+			}
 		}
 	}
 
--- service/file/fileServer_test.go
+++ service/file/fileServer_test.go
@@ -8,7 +8,7 @@ import (
 	"testing"
 )
 
-func TestGetFilesAndDirs(t *testing.T) {
+func TestFileServer_GetFilesAndDirs(t *testing.T) {
 	c := brick.NewContainer()
 	c.Add(&brick.JSONConfigService{}, "config", brick.FactoryFunc(func() interface{} {
 		return brick.NewJSONConfigService("/Users/liyuxin/go/src/selfText/giligili_back/cmd/ntpServer/config/config.json")
@@ -24,13 +24,95 @@ func TestGetFilesAndDirs(t *testing.T) {
 
 	dirPath := "/Users/liyuxin/Documents/工作/网太/文档"
 	fileServer := c.GetByName("FileServer").(*FileServer)
-	files, dirs, err := fileServer.GetFilesAndDirs(dirPath, "", 1)
+	files, dirs, err := fileServer.GetFilesAndDirs(dirPath, "", 2)
 	if err != nil {
 		t.Error(err)
 	}
 	t.Log("print files:")
 	for _, file := range files {
 		t.Log(file)
+	}
+	t.Log("print dirs:")
+	for _, dir := range dirs {
+		t.Log(dir)
+	}
+}
+
+func TestFileServer_GetLimitFilesAndDirs(t *testing.T) {
+	c := brick.NewContainer()
+	c.Add(&brick.JSONConfigService{}, "config", brick.FactoryFunc(func() interface{} {
+		return brick.NewJSONConfigService("")
+	}))
+
+	c.Add(&orm.OrmService{}, "OrmService", nil)
+	c.Add(&mysql.MySQLService{}, "DB", nil)
+	c.Add(&logging.LoggerService{}, "LoggerService", nil)
+
+	c.Add(&FileServer{}, "FileServer", nil)
+
+	c.Build()
+
+	dirPath := ""
+	fileServer := c.GetByName("FileServer").(*FileServer)
+	files, dirs, err := fileServer.GetLimitFilesAndDirs(dirPath, "")
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log("print files:")
+	for _, file := range files {
+		t.Log(file)
+	}
+	t.Log("print dirs:")
+	for _, dir := range dirs {
+		t.Log(dir)
+	}
+}
+
+func TestFileServer_GetAllFiles(t *testing.T) {
+	c := brick.NewContainer()
+	c.Add(&brick.JSONConfigService{}, "config", brick.FactoryFunc(func() interface{} {
+		return brick.NewJSONConfigService("")
+	}))
+
+	c.Add(&orm.OrmService{}, "OrmService", nil)
+	c.Add(&mysql.MySQLService{}, "DB", nil)
+	c.Add(&logging.LoggerService{}, "LoggerService", nil)
+
+	c.Add(&FileServer{}, "FileServer", nil)
+
+	c.Build()
+
+	dirPath := ""
+	fileServer := c.GetByName("FileServer").(*FileServer)
+	files, err := fileServer.GetAllFiles(dirPath, "")
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log("print files:")
+	for _, file := range files {
+		t.Log(file)
+	}
+}
+
+func TestFileServer_GetAllDirs(t *testing.T) {
+	c := brick.NewContainer()
+	c.Add(&brick.JSONConfigService{}, "config", brick.FactoryFunc(func() interface{} {
+		return brick.NewJSONConfigService("")
+	}))
+
+	c.Add(&orm.OrmService{}, "OrmService", nil)
+	c.Add(&mysql.MySQLService{}, "DB", nil)
+	c.Add(&logging.LoggerService{}, "LoggerService", nil)
+
+	c.Add(&FileServer{}, "FileServer", nil)
+
+	c.Build()
+
+	dirPath := ""
+	fileServer := c.GetByName("FileServer").(*FileServer)
+	dirs, err := fileServer.GetAllDirs(dirPath, 2)
+	if err != nil {
+		t.Error(err)
 	}
 	t.Log("print dirs:")
 	for _, dir := range dirs {
